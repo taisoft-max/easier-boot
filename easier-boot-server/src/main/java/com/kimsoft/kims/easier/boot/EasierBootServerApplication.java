4d68be9777315b7e9cdbbe7082f50223ce919060
kimizhang <kimizhang@swireproperties.com>
2020-05-06 10:16:05 +0800

    generator add config

--- easier-boot-server/src/main/java/com/kimsoft/kims/easier/boot/EasierBootServerApplication.java
+++ easier-boot-server/src/main/java/com/kimsoft/kims/easier/boot/EasierBootServerApplication.java
@@ -1,9 +1,11 @@
 package com.kimsoft.kims.easier.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kimsoft.kims.easier.boot.user.mapper")
 public class EasierBootServerApplication {
 
     public static void main(String[] args) {
